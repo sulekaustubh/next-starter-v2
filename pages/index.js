6e14680f7899ac5fe2b7715e3b2f9b0e8cc54177
sulekaustubh <sulekaustubh97@gmail.com>
2023-10-19 15:56:03 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// import { Inter } from 'next/font/google';
+import { Button } from '@material-tailwind/react';
+// const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<main className="text-2xl">
+			<div>Hey World !</div>
+			<div>
+				{/* <Button>B</Button> */}
+			</div>
+		</main>
+	);
+}
